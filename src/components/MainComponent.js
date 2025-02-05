259a61e006f4bed6de4a2fdd0fbed27fbc6b4f09
Cesar Mendoza <cesar.mendoza.77@gmail.com>
2025-02-05 11:43:06 -0300

    pruebaa

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,8 +4,8 @@ import React from 'react';
 function MainComponent() {
   return (
     <div style={styles.main}>
-      <h1>Frame Trasa</h1>
-      <iframe title="Tarjetas 2025" width="800" height="636" src="https://app.powerbi.com/view?r=eyJrIjoiNjRmMWY1MjUtOGZiNy00ZjI2LTgwZDgtZjljMmI0NzNiODEzIiwidCI6ImE2ZDNmNWMyLWYyMWYtNGIyYi04MTEwLTMxM2FmZGFhNmY0MiJ9" frameborder="0" allowFullScreen="true"></iframe>
+      <h1>main</h1>
+      
       
     </div>
   );
